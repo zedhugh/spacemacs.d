d72d0fb8f1cef15d87764ace923d3517ce9c2035
Zedhugh Chen <zedhugh@gmail.com>
2018-01-23 14:24:34 +0800

    change keybinding.
    1. delete indent function defined by myself.
    2. delete multi-cursors edit function.
    3. add copy content to clipboard.

--- layers/czh/keybindings.el
+++ layers/czh/keybindings.el
@@ -11,14 +11,13 @@
 
 (global-set-key (kbd "M-s o") 'czh/occur-dwin)
 
-(global-set-key (kbd "C-M-\\") 'czh/indent-region-or-buffer)
+(global-set-key (kbd "C-M-\\") 'spacemacs/indent-region-or-buffer)
 
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
 (global-set-key (kbd "s-v") 'clipboard-yank)
-
-(global-set-key (kbd "s-d") 'czh/my-mc-mark-next-like-this)
+(global-set-key (kbd "s-c") 'clipboard-kill-ring-save)
 
 (spacemacs/set-leader-keys "oy" 'youdao-dictionary-search-at-point+)
 (spacemacs/set-leader-keys "op" 'youdao-dictionary-play-voice-at-point)
